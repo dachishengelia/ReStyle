05d953b02871ba876eff435aec4dbebb551920fc
gikraz <130279660+gikraz@users.noreply.github.com>
2025-10-11 16:00:31 +0400

    update

---
+++ frontend/src/api.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+const API = axios.create({
+  baseURL: 'https://re-style-backend.vercel.app',
+});
+
+export const registerUser = (data) => API.post('/auth/register', data);
+export const loginUser = (data) => API.post('/auth/login', data);
--- src/api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const API = axios.create({ baseURL: 'http://localhost:5000/api' });
-
-export const registerUser = (data) => API.post('/auth/register', data);
-export const loginUser = (data) => API.post('/auth/login', data);
-export const addProduct = (data, token) => API.post('/products', data, { headers: { Authorization: `Bearer ${token}` } });
-export const getProducts = () => API.get('/products');
