da91956cb84c8d689fddb1de8cca1b5ce34d6163
gikraz <130279660+gikraz@users.noreply.github.com>
2025-10-18 02:48:01 +0400

    first commit

--- src/api.js
+++ src/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 
-const API_URL = "https://re-style-backend.onrender.com/api/auth";
-
+const API_URL = "https://vercel.com/gikrazs-projects/backend-testing";
 
 export const loginUser = (data) => axios.post(`${API_URL}/login`, data);
 export const registerUser = (data) => axios.post(`${API_URL}/register`, data);
